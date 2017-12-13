24cbb27aeb72f1e0c5c67158f193cd0154735150
Yanhao Li <hi@yanhaoli.com>
2017-12-13 17:40:03 -0500

    added login signup components

---
+++ src/app/containers/auth/signup/signup.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { SignupComponent } from './signup.component';
+
+@NgModule({
+  declarations: [
+    SignupComponent
+  ],
+  exports: [
+    SignupComponent
+  ]
+})
+
+export class SignupModule {}
