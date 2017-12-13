5b56601c1d7296b44053de7942d435df89dd4830
Yanhao Li <hi@yanhaoli.com>
2017-12-13 17:28:13 -0500

    added navbar gridlist

--- src/app/components/navbar/navbar.module.ts
+++ src/app/components/navbar/navbar.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { NavbarComponent } from './navbar.component';
 
@@ -10,7 +12,8 @@ import { NavbarComponent } from './navbar.component';
     MatToolbarModule,
     MatSidenavModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatGridListModule
   ],
   declarations: [
     NavbarComponent
