1f9b23dfc4ae564116598d2c4835cd1bb651695a
Yanhao Li <hi@yanhaoli.com>
2017-12-13 17:57:35 -0500

    created login signup components

--- src/app/containers/auth/login/login.module.ts
+++ src/app/containers/auth/login/login.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { LoginComponent } from './login.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
+  imports: [
+    MatCardModule
+  ],
   declarations: [
     LoginComponent
   ],
