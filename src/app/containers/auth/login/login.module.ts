52fe2c97c1c6a7807ea0d4fb019f4cd3c11fbd03
Yanhao Li <hi@yanhaoli.com>
2017-12-14 10:40:09 -0500

    finished logging page style

--- src/app/containers/auth/login/login.module.ts
+++ src/app/containers/auth/login/login.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
-import { LoginComponent } from './login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { LoginComponent } from './login.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
-    MatCardModule
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterModule
   ],
   declarations: [
     LoginComponent
