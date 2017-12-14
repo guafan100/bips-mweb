0097dc275812fc6757c781d09b2560738d82d13d
Yanhao Li <hi@yanhaoli.com>
2017-12-14 11:03:03 -0500

    created auth service

---
+++ src/app/core/services/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class AuthService {
+  constructor(
+
+  ) {}
+
+  login(credential): Observable<any> {
+    return 
+  }
+}
