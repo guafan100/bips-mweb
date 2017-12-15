a4c42b2bc89333c3ffd770c7f50df690affb827f
Will Luo <453117947@qq.com>
2017-12-15 16:32:04 -0500

    will2

--- src/app/components/navbar/current-user/current-user.component.ts
+++ src/app/components/navbar/current-user/current-user.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnChanges } from '@angular/core';
 import { User } from 'app/core/models';
 import * as fromAuthReduer from 'app/core/reducers/auth.reducer';
+import { PACKAGE_ROOT_URL } from '@angular/core/src/application_tokens';
 
 @Component({
   selector: 'current-user',
@@ -12,12 +13,13 @@ export class CurrentUserComponent implements OnChanges {
   @Input() auth: fromAuthReduer.State;
 
   constructor() {
+  }
 
+  onInit() {
   }
 
   ngOnChanges() {
 
   }
-
 
 }
